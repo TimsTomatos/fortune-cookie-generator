7ff910e2d37be0352b3c0c6ac1f5b7a2655759e3
TimsTomatos <30964932+TimsTomatos@users.noreply.github.com>
2018-02-22 21:36:57 -1000

    lol

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ function forTell() {
 var fortune = ["test","quiz","hw","cw"];
 
 
-return fortune[Math.floor(Math.random() * fortune.length)]
+ var f = Math.floor((Math.random() * fortune.length));
+
+document.getElementById("unfortune").innerHTML = fortune[f]
+
+var allFortune = document.createElement("LI");
+var allOfFortune = document.createTextNode(fortune[f]);
+allFortune.appendChild(allOfFortune);
+document.getElementById("oldFort").appendChild(allFortune);
+//fortune.indexOf(document.getElementById("unfortune").innerHTML)
+fortell.splice(f,1)
 }
-console.log(forTell());
+//if fortune == 0 
+
+
